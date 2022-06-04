6f34c49d95e9e5a875ebaa80d2869d7af01df68e
Alim <alim.filipov@gmail.com>
2022-06-04 20:48:35 +0300

    TM-1 mongodb is ready

--- mail-sender/src/main/java/com/alim/mailsender/MailSenderApplication.java
+++ mail-sender/src/main/java/com/alim/mailsender/MailSenderApplication.java
@@ -4,32 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class MailSenderApplication implements CommandLineRunner {
+@EnableMongoRepositories
+public class MailSenderApplication {
 
-    private final JavaMailSender javaMailSender;
+//    private final JavaMailSender javaMailSender;
 
     public static void main(String[] args) {
         SpringApplication.run(MailSenderApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) {
-        System.out.println("Sending Email...");
-        sendEmail();
-        System.out.println("Done");
-    }
-
-
-    void sendEmail() {
-        SimpleMailMessage sms = new SimpleMailMessage();
-        sms.setTo("alim.filipov@gmail.com");
-        sms.setSubject("Я приду похаваю");
-
-        javaMailSender.send(sms);
-    }
+//    @Override
+//    public void run(String... args) {
+//        System.out.println("Sending Email...");
+//        sendEmail();
+//        System.out.println("Done");
+//    }
+//
+//
+//    void sendEmail() {
+//        SimpleMailMessage sms = new SimpleMailMessage();
+//        sms.setTo("alim.filipov@gmail.com");
+//        sms.setSubject("Я приду похаваю");
+//
+//        javaMailSender.send(sms);
+//    }
 }
---
+++ mail-sender/src/main/java/com/alim/mailsender/dto/request/EmailRequest.java
@@ -0,0 +1,11 @@
+package com.alim.mailsender.dto.request;
+
+import lombok.Data;
+
+@Data
+public class EmailRequest {
+
+    private String id;
+    private String name;
+    private String email;
+}
