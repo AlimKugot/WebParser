806d8f50ad97bedb7067d2fe2a92e6f0e0f51698
Alim <alim.filipov@gmail.com>
2022-07-24 14:29:10 +0300

    RF fixed tests with docker-compose.yaml

--- admin/src/test/java/com/alim/admin/AdminApplicationTests.java
+++ admin/src/test/java/com/alim/admin/AdminApplicationTests.java
@@ -1,13 +1,21 @@
 package com.alim.admin;
 
+import com.alim.admin.controller.AdminDefaultController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AdminApplicationTests {
 
+    @Autowired
+    private AdminDefaultController controller;
+
     @Test
     void contextLoads() {
+        assertNotNull(controller);
     }
 
 }
